9fb8ab1d7aaafab2e9a68770c8f1eb62b59a7419
Yan <service@yanlx.com>
2026-02-25 12:25:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const { init: initDB, Counter } = require("./db");
+const { init: initDB, Counter,TeaInvitation } = require("./db");
 
 const logger = morgan("tiny");
 
